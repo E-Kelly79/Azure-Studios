c8e534f29e8e03ffe922881be2902d1a9adf4d8f
Eoin <k.eoin@yahoo.ie>
2017-10-23 20:00:00 +0100

    Introduced FileLogger Class

--- src/controller/Azure.java
+++ src/controller/Azure.java
@@ -4,13 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import model.Users;
+import utils.FileLogger;
 
 public class Azure {
 
 	public static void main(String[] args) {
+		FileLogger logger = FileLogger.getLogger();
+		logger.log("Creating User List");
+		
 		List<Users> users = new ArrayList<>();
 		users.add(new Users("Eoin", "Kelly", 38, 'M', "Student"));
 		System.out.println(users);
+		
+		logger.log("Finished - shutting Down");
 
 	}
 
